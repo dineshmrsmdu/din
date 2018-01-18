736993c68958b6ccfde2e1e82d0683c94fbd07d9
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-01-18 11:24:58 +0000

    Create display.c

---
+++ display.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the number:");
+scanf(%d%d,&m,&n);
+printf("all odd number from %d to %d are\n",m,n);
+for(a=0;a<b;a++)
+{
+  if(a%2!=0)
+  {
+    prtintf("%d\n");
+  }
+}
+}
