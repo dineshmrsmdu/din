866f59af19999bea0e863753ec130eef9d9e3d17
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-01-18 11:19:25 +0000

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,i;
+	int count=1;
+printf("enter the numbers");
+scanf("%d\t%d",&a,&b);
+for(i=1;i<=b;i++)
+{
+	c=count*a;
+	count=c;
+}
+printf("%d",c);
+return 0;
+}
