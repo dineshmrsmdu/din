941fc68b629794c162210a73b6d479f641014b9b
dineshmrsmdu <35331305+dineshmrsmdu@users.noreply.github.com>
2018-01-18 11:08:27 +0000

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+printf("Enter the number of times:");
+scanf("%d",&n);
+if(n>0)
+{
+for(i=1;i<=n;i++)
+{
+printf("Hello \n");
+}
+else
+ printf("invalid input");
+}
+}
+
